a458b867eed3e2baeca584898ebbc0957a517e1a
Brennan <brennan.moeller@gmail.com>
2022-11-03 11:20:25 -0600

    added if __name__ == __main__ adn the test_image rename file

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -2,7 +2,11 @@ import unittest
 import random
 import numpy as np
 import sys
-sys.path.append('src/ippsra')
+sys.path.append('./src/ippsra')
 import utils  # nopep8
 
 utils.test('this tes_utils ran')
+
+
+if __name__ == '__main__':
+    unittest.main()
