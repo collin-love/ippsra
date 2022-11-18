1444b7d92d6cc22601837a5df69a1f79919a447b
Blmoeller <43049946+Blmoeller@users.noreply.github.com>
2022-11-18 08:52:00 -0700

    Update to pycode style

--- src/ippsra/rank_images.py
+++ src/ippsra/rank_images.py
@@ -10,7 +10,6 @@ sys.path.append('./src/ippsra')
 from image_processing_funcs import ImageAnalysis  # nopep8
 
 
-
 def get_args():
 
     parser = argparse.ArgumentParser(description='Code for ranking a '
@@ -24,11 +23,12 @@ def get_args():
     parser.add_argument('--Extension', '-ext', default='png',
                         help='A list of the extensions for the image data '
                         + 'that is present in the directory for processing',
-                        choices=['bmp', 'dib', 'jpeg', 'jpg', 'png', 'webp', 
-                                 'pbm', 'pgm', 'ppm', 'pxm', 'pnm', 'sr', 
+                        choices=['bmp', 'dib', 'jpeg', 'jpg', 'png', 'webp',
+                                 'pbm', 'pgm', 'ppm', 'pxm', 'pnm', 'sr',
                                  'ras', 'tiff', 'tif', 'exr', 'hdr', 'pic'])
 
     return parser.parse_args()
+
 
 def rank_images():
     """
@@ -47,7 +47,7 @@ def rank_images():
         raise OSError("This directory is empty")
 
     # iterate through the directory and rank the images
-    
+
     for ext in os.listdir(imageDir):
         imagePath = os.path.join(imageDir, ext)
 
@@ -56,43 +56,8 @@ def rank_images():
         print(ImageAnalysis().density_hazards(img=imagePath, threshold=200))
         print(ImageAnalysis().hazard_score(img=imagePath, threshold=200))
 
-
-
     # ImageAnalysis().show_img(img=imagePath)
     # ImageAnalysis().show_bbox(img=imagePath)
-
-        
-        
-        
-    # Ims = []  # Setting up a list to add image data to
-    # [Ims.extend(glob.glob(corpus_dir + '*.' + e)) for e in ext]
-
-    # images = [cv2.imread(file) for file in Ims]
-
-'''
-def argparser(imageDir, ext=['png', 'jpg']):
-    """Function to iterate through a directory of image data
-
-    Args:
-        imageDir (str): Directory for the dataset.
-        ext (list, optional): Different image formats to check for. Defaults
-        to ['png', 'jpg'].
-    """
-    # Checking if the directory exists
-    if os.path.exists(imageDir) is False:
-        raise OSError("This directory is empty")
-    # Checking if the dir is empty or not
-    dir = os.listdir(imageDir)  # listing the contents
-    if len(dir) == 0:
-        raise OSError("This directory is empty")
-
-    Is = []  # Setting up a list to add image data to
-    [Is.extend(glob.glob(imageDir + '*.' + e)) for e in ext]
-
-    images = [cv2.imread(file) for file in Is]
-
-    return images
-'''
 
 
 if __name__ == '__main__':
