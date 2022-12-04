238daba09b63f34490056647b13e30924c0fc480
Blmoeller <43049946+Blmoeller@users.noreply.github.com>
2022-12-04 09:52:58 -0700

    Updated func test to check for outputs

--- tests/func/test_func.sh
+++ tests/func/test_func.sh
@@ -18,7 +18,33 @@ assert_exit_code 0
 run test_in_stderr python src/ippsra/rank_images.py --save True --showPlot False  --showImages False
 assert_in_stderr "OSError: (OSError): The file sorted_test.csv already exists in the directory ./data/processed_data. Please move or delete the file and try again"
 
-rm ./data/processed_data/*.csv && rm ./data/processed_data/*.png
+# Checking if the files were created and if they were deleted
+if [[ -e data/processed_data/raw_data.csv ]] && [[ -e data/processed_data/sorted_test.csv ]]; then
+    rm ./data/processed_data/raw_data.csv
+    rm ./data/processed_data/sorted_test.csv
+fi
+
+for f in data/processed_data/*.png
+do
+    if [[ -e $f ]]; then
+        rm $f
+    fi
+done
+
+for f in data/processed_data/*.csv
+do
+    if [[ -f $f ]]; then
+        echo "File $f does exist"; else echo "Files $f do not exist they were deleted"
+    fi
+done
+
+for f in data/processed_data/*.png
+do
+    if [[ -f $f ]]; then
+        echo "File $f does exist"; else echo "Files $f do not exist they were deleted"
+    fi
+done
+
 
 run test_in_stderr python src/ippsra/rank_images.py --save False --showPlot False --showImages False --Directory ./data/FAKE_Directory
 assert_in_stderr "No such file or directory: './data/FAKE_Directory'"
